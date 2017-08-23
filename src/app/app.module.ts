3e496677c53226fd1bf8f80be4c4ff84da74b468
DESKTOP-4F9NOBO\User <andersonlavor@gmail.com>
2017-08-23 06:40:13 -0400

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import {MenubarModule, ButtonModule} from 'primeng/primeng';
+
+
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './common/home/home.component';
+import { DashboardComponent } from './common/dashboard/dashboard.component';
+import { NavbarComponent } from './common/home/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MenubarModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/common/home/navbar/navbar.component.ts
@@ -0,0 +1,135 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/primeng';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  items: MenuItem[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.items = [{
+      label: 'Order Processing',
+      items: [{
+        label: 'Manage New Order'
+      }, {
+        label: 'Manage Received Order'
+      }, {
+        label: 'Order Cancellation'
+      }, {
+        label: 'Manage Mispostings'
+      }, {
+        label: 'Manual Posting of Order'
+      }, {
+        label: 'Manage Delivered Order'
+      }, {
+        label: 'Regenerate Order Emails'
+      }]
+  },{
+      label: 'Settings',
+      items: [{
+        label: 'Setup Delivery Day'
+      }, {
+        label: 'Setup Delivery Alert'
+      }, {
+        label: 'Setup Client Rules'
+      }, {
+        label: 'Setup Client Product Rules'
+      }, {
+        label: 'Accepted Payment Option'
+      }, {
+        label: 'Manage Content'
+      }, {
+        label: 'Setup Tank Size Specs'
+      }, {
+        label: 'Hint Question'
+      }, {
+        label: 'Setup Credit'
+      }, {
+        label: 'Client Newsletter Options'
+      }, {
+        label: 'Setup Upsell Product'
+      }, {
+        label: 'Manage Product'
+      }, {
+        label: 'Behavioral Targeting'
+      }, {
+        label: 'Google Analytic Details'
+      }, {
+        label: 'Manage Source'
+      }, {
+        label: 'Google Analytic Targets'
+      }, {
+        label: 'KPI Benchmark(beta'
+      }, {
+        label: 'Facebook Appid'
+      }, {
+        label: 'Setup Highlight Option'
+      }],
+    }, {
+      label: 'Pricing',
+      items: [{
+        label: 'Pricing Region Maintenance'
+      }, {
+        label: 'Price Maintenance'
+      }, {
+        label: 'Setup Tax'
+      }, {
+        label: 'Edit Tax'
+      }]
+    }, {
+      label: 'Manage Account',
+      items: [{
+        label: 'Register Customer'
+      }, {
+        label: 'Credit On Account Assign'
+      }, {
+        label: 'Credit On Account UnAssign'
+      }, {
+        label: 'CSR/Dealer User Setup'
+      }, {
+        label: 'Search CSR/Dealer'
+      }]
+    }, {
+      label: 'Manage Promotions',
+      items: [{
+        label: 'Add Promotions'
+      }, {
+        label: 'Edit/Manage Promotions'
+      }, {
+        label: 'Add/Remove Promo on Order'
+      }]
+    }, {
+      label: 'Help',
+      items: [{
+        label: 'Help Desk'
+      }, {
+        label: 'How To'
+      }, {
+        label: 'Enhancements -Sell More'
+      }, {
+        label: 'Enhancements -Engine Easy'
+      }]
+    }]
+  }
+
+}
+// {
+//   label: 'File',
+//     items: [{
+//   label: 'New',
+//   icon: 'fa-plus',
+//   items: [
+//     {label: 'Project'},
+//     {label: 'Other'},
+//   ]
+// },
+//   {label: 'Open'},
+//   {label: 'Quit'}
+// ]
+// },
